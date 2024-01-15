b5375b269c998bda1a91b88edbb9923871f9cad1
Chris H <chg@live.cl>
2024-01-15 16:32:36 +1300

    fix database 2

--- recipes/settings.py
+++ recipes/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'recipes.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 # Use SQLite for local development
 if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    }
+    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 else:
-    # Assuming you are using SQLite for local development
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
